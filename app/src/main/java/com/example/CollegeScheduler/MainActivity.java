4340b6cd1efaf78eb78d9d5e3b9bb404ad188349
yoshspider <yash131072@gmail.com>
2024-01-25 13:43:55 -0500

    renamed views

--- app/src/main/java/com/example/CollegeScheduler/MainActivity.java
+++ app/src/main/java/com/example/CollegeScheduler/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends Activity {
         simpleList.setAdapter(classAdapter);
         addButtonFunctionality();
         sortButtonFunctionality();
-
+        
 
     }
 
