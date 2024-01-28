0e2afdf27876fd946f9b66dcaa9094d10af90b88
TanglyEagle7718 <rishi.borra@outlook.com>
2024-01-27 22:10:04 -0500

    Revert "Merge branch 'main' into addmodifyscreen"
    This reverts commit 34fad28147f7b59c3112ef8ecd51587e79b952a6, reversing
    changes made to bb1708edb45ed4b3c0f8adb87c89ca59b7b4354a.
    
    	modified:   .idea/.name
    	modified:   app/src/main/java/com/example/CollegeScheduler/Assignment.java
    	modified:   app/src/main/java/com/example/CollegeScheduler/Class.java
    	modified:   app/src/main/java/com/example/CollegeScheduler/ClassAdapter.java
    	modified:   app/src/main/java/com/example/CollegeScheduler/CollegeObjectList.java
    	modified:   app/src/main/java/com/example/CollegeScheduler/Exam.java
    	modified:   app/src/main/java/com/example/CollegeScheduler/ListItem.java
    	modified:   app/src/main/java/com/example/CollegeScheduler/MainActivity.java
    	deleted:    app/src/main/java/com/example/CollegeScheduler/Task.java
    	modified:   app/src/main/res/layout/activity_listview.xml
    	modified:   app/src/main/res/layout/fragment_classes.xml
    	deleted:    app/src/main/res/layout/fragment_tasks.xml

--- app/src/main/java/com/example/CollegeScheduler/Assignment.java
+++ app/src/main/java/com/example/CollegeScheduler/Assignment.java
@@ -1,90 +1,38 @@
 package com.example.CollegeScheduler;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-
-import java.util.Date;
-
 public class Assignment extends ListItem {
     private String assignmentName;
     private Class classOfAssignment;
+    private int monthDue;
+    private int dayDue;
 
-    /**
-     * Base Constructor for Assignment
-     * @param assignmentName Name of Assignment
-     * @param classOfAssignment Associated Class Object of Assignment
-     * @param yearDue Year the Assignment is Due
-     * @param monthDue Month the Assignment is Due
-     * @param dayDue Day the Assignment is Due
-     * @param hourDue Hour the Assignment is Due
-     * @param minuteDue Minute the Assignment is Due
-     */
-    public Assignment(String assignmentName, Class classOfAssignment, Calendar time) {
-        super(time);
+    public Assignment(String assignmentName, Class classOfAssignment, int monthDue, int dayDue) {
         this.assignmentName = assignmentName;
         this.classOfAssignment = classOfAssignment;
-        setNameSort(classOfAssignment.getClassName() + "1" + assignmentName);
-        setColor(classOfAssignment.getColor());
-
+        this.dayDue = dayDue;
+        this.monthDue = monthDue;
 
     }
 
-
-    /**
-     * Getter for Assignment Name
-     * @return the name of the Assignment
-     */
+    //comment
     public String getAssignmentName() {
         return assignmentName;
     }
 
-    /**
-     * Getter for class of Assignment
-     * @return the class object of Assignment
-     */
     public Class getClassOfAssignment() {
         return classOfAssignment;
     }
 
-    /**
-     * Setter for Assignment Name
-     * @param assignmentName New Name for Assignment
-     */
-    public void setAssignmentName(String assignmentName) {
-        this.assignmentName = assignmentName;
+    public int getDayDue() {
+        return dayDue;
     }
 
-    /**
-     * Setter for Class of Assignment
-     * @param classOfAssignment New Class Object for Assignment
-     */
-    public void setClassOfAssignment(Class classOfAssignment) {
-        this.classOfAssignment = classOfAssignment;
+    public int getMonthDue() {
+        return monthDue;
     }
 
-    /**
-     * Draw Instructions for Assignment
-     * @param view View of which the assignment will be draw
-     * @param inflater LayoutInflater that clears View
-     * @param i Index of Assignment Object in
-     * @param adapter the ClassAdapter Object that the item is being drawn with
-     * @return the modified View object
-     */
     @Override
-    public View drawScreen(View view, LayoutInflater inflater, int i, ClassAdapter adapter) {
-        view = super.drawScreen(view, inflater, i, adapter);
-        String date = getTime();
-        int splitIndex = date.indexOf(" ");
-        String[] values = {getAssignmentName(), getClassOfAssignment().getClassName(),
-                date.substring(0, splitIndex), date.substring(splitIndex + 1)};
-        view = drawInformation(view, values);
-        view = drawButtons(view, inflater, i, adapter);
-        return view;
+    public int compareTo(ListItem o) {
+        return ((Assignment)o).getClassOfAssignment().compareTo(classOfAssignment);
     }
-
 }
--- app/src/main/java/com/example/CollegeScheduler/ClassAdapter.java
+++ app/src/main/java/com/example/CollegeScheduler/ClassAdapter.java
@@ -10,61 +10,31 @@ import android.widget.TextView;
 
 public class ClassAdapter extends BaseAdapter {
     Context context;
-    CollegeObjectList<ListItem> itemsList;
+    CollegeObjectList<Class> itemsList;
+
     LayoutInflater inflater;
 
-    /**
-     * Constructor for ClassAdapter Object
-     * @param applicationContext context of App given by Main Activity
-     * @param itemsList the CollegeObjectList which contains a number of ListItem objects
-     *                  that will be drawn by ClassAdapter
-     */
-    public ClassAdapter(Context applicationContext, CollegeObjectList<ListItem> itemsList) {
+    public ClassAdapter(Context applicationContext, CollegeObjectList<Class> itemsList) {
         this.context = applicationContext;
         this.itemsList = itemsList;
         inflater = (LayoutInflater.from(applicationContext));
     }
-    /**
-     * Getter for size of list
-     * @return the size of the list
-     */
+
     @Override
     public int getCount() {
         return itemsList.size();
     }
-    /**
-     *
-     * gets the ListItem object at index i
-     * @param i Position of the item whose data we want within the adapter's
-     * data set.
-     * @return ListItem object at index i
-     */
+
     @Override
     public Object getItem(int i) {
-        return itemsList.getItem(i);
+        return null;
     }
-    /**
-     * getter for the id of associated List Item
-     * @param i The position of the item within the adapter's data set whose row id we want.
-     * @return the ID of ListItem object at index i
-     */
+
     @Override
     public long getItemId(int i) {
-        return itemsList.getItem(i).getID();
+        return 0;
     }
-    /**
-     * Creates the View and Draws it Accordingly for a given index in the list
-     * @param i The position of the item within the adapter's data set of the item whose view
-     *        we want.
-     * @param view The old view to reuse, if possible. Note: You should check that this view
-     *        is non-null and of an appropriate type before using. If it is not possible to convert
-     *        this view to display the correct data, this method can create a new view.
-     *        Heterogeneous lists can specify their number of view types, so that this View is
-     *        always of the right type (see {@link #getViewTypeCount()} and
-     *        {@link #getItemViewType(int)}).
-     * @param viewGroup The parent that this view will eventually be attached to
-     * @return modified view with correct features
-     */
+
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
         view = inflater.inflate(R.layout.activity_listview, null);
@@ -113,14 +83,5 @@ public class ClassAdapter extends BaseAdapter {
     public String daysOfTheWeekConverter(boolean[] days) {
         String week = (days[0] ? "M/":"") + (days[1] ? "T/":"") + (days[2] ? "W/":"") + (days[3] ? "TR/":"") + (days[4] ? "F/":"");
         return week.substring(0, week.length()-1);
-        //return itemsList.getItem(i).drawScreen(view, inflater, i, this);
     }
-  /**
-     * remove an index from the list and update drawing
-     * @param i the index of the object removed
-     */
-  public void remove(int i) {
-        itemsList.removeItem(i);
-        notifyDataSetChanged();  
-  }
 }
