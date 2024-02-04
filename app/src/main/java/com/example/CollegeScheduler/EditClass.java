d2c2fafd6323d618807529e243b060357e27abbb
TanglyEagle7718 <rishi.borra@outlook.com>
2024-02-04 06:53:18 -0500

    Selecting class will set text for course name to the name of the class selected

--- app/src/main/java/com/example/CollegeScheduler/EditClass.java
+++ app/src/main/java/com/example/CollegeScheduler/EditClass.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -48,15 +49,23 @@ public class EditClass extends Fragment implements AdapterView.OnItemSelectedLis
         //get the spinner from the xml.
         Spinner dropdown = getView().findViewById(R.id.spinner1);
         //create a list of items for the spinner.
-        ArrayList<String> list = new ArrayList<>();
-        list.add("0000");
-        list.add("0001");
-        list.add("0010");
-        list.add("0011");
+
+
+        //Create a list of Class objects. Based on this list, create a list of strings w/ names of classes
+        // Use this list as input for the spinner. When selecting item, get index of item
+        ArrayList<Class> list_of_classes = new ArrayList<>();
+        list_of_classes.add(new Class("Math", "Mcfadden", new boolean[]{true, false, true, false, true}, 700, 900, "CULC 250"));
+        list_of_classes.add(new Class("Chemistry", "Allshouse", new boolean[]{false, true, false, true, false}, 800, 1000, "CULC 250"));
+        list_of_classes.add(new Class("Objects and Design", "Pedro", new boolean[]{false, true, false, true, false}, 1230,1430, "HOWEY A419"));
+
+        ArrayList<String> list_of_class_names = new ArrayList<>();
+        for (Class classObject : list_of_classes) {
+            list_of_class_names.add(classObject.toString());
+        }
 
         //create an adapter to describe how the items are displayed, adapters are used in several places in android.
         //There are multiple variations of this, but this is the basic variant.
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(classActivity.getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(classActivity.getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, list_of_class_names);
         //set the spinners adapter to the previously created one.
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         dropdown.setAdapter(adapter);
@@ -65,8 +74,12 @@ public class EditClass extends Fragment implements AdapterView.OnItemSelectedLis
         dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                @Override
                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                   String item = dropdown.getItemAtPosition(position).toString();
-                   System.out.println(item);
+                   System.out.println(position);
+                   Class item_changed = list_of_classes.get(position);
+                   final EditText course_name = (EditText) getView().findViewById(R.id.course_entry);
+
+                   //course_name.setHint(item_changed.getClassName());
+                   course_name.setText(item_changed.getClassName());
                }
 
                @Override
